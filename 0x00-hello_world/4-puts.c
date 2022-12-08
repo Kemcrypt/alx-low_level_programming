8c4d3d643f33282778ef30a592194d5599a860a3
Kemcrypt <kemiola89@yahoo.com>
2022-12-08 14:14:08 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
- #include <stdio.h>                                                                                                                      
-/**                                                                                                                                     
- * main - Entry                                                                                                                         
- * Return: Always 0                                                                                                                     
- */                                                                                                                                     
-int main(void)                                                                                                                          
-{                                                                                                                                       
-        puts("\"Programming is like building a multi lingual puzzle);                                                            
-        return (0);
-}
+#include <stdio.h>
+/*
+ * main - print a line of code using puts function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	Return (0);
+}	
+

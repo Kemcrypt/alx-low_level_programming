d04bbc9af2fad945eaafdaade7b0d21ae9cb1a28
Kemcrypt <kemiola89@yahoo.com>
2022-12-09 07:42:17 -0800

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,11 +22,11 @@ int main(void)
 	}
 	if (x == 0)
 	{
-		printf(" and is 0");
+		printf("and is 0");
 	}
 	if (x < 6 && x != 0)
 	{
-		printf(" and is less than 6 and not 0");
+		printf("and is less than 6 and not 0");
 	}
 	printf("\n");
 	return (0);

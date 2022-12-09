eed2d9fc900086eab57317e1fd154f47577765fa
Kemcrypt <kemiola89@yahoo.com>
2022-12-09 11:42:01 -0800

    print comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,20 +10,20 @@ int main(void)
 	int ones = '0';
 	int tens = '0';
 
-	if (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
+	for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
 	{
 		for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
 		{
-			if (!((ones == tens) || (tens > ones)))/*eliminates repitition*/
-			{
-					putchar(tens);
-					putchar(ones);
-					if (!(ones == '9' && tens == '8'))/*addes commma and space*/
-					{
-						putchar(',');
-						putchar(' ');
-					}
-			}
+		if (!((ones == tens) || (tens > ones)))/*eliminates repitition*/
+		{
+			putchar(tens);
+			putchar(ones);
+		if (!(ones == '9' && tens == '8'))/*addes commma and space*/
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		}
 		}
 	}
 

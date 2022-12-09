e4bd76b85a60297e8cc6053d5a0cb82324fc748d
Kemcrypt <kemiola89@yahoo.com>
2022-12-09 04:53:24 -0800

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 	if (m > 5)      
 		printf("last digit of %d is %d and is greater than 5\n", n, m);                                                               if (m == 0)                                      
 		printf("last digit of %d is %d and is 0\n", n, m);
-	if (m < 6 && m |= 0) 
+	if (m < 6 && m != 0) 
 		printf("last digit of %d is %d and is less than 6 and not 0\n", n, m);
 	return (0);
 }	

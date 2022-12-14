c69a3e366907a60116ea97dab183639326fc4b8a
Kemcrypt <kemiola89@yahoo.com>
2022-12-13 17:14:17 -0800

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,30 +5,26 @@
 
 void times_table(void)
 {
-	int i, j, k;
+	int i, j, prod;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		k = j * i;
-		if (j == 0)
-		{
-			_putchar(k + '0');
-		}
 
-			if (k < 10 && j != 0)
-			{
-				_putchar(',');
+		-putchar('0');
+
+		for (j = 1; j <= 9; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = i * j;
+
+			if (prod <= 9)
 				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-			} else if (k >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
-			}
+			else
+				_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
-	{
+	}
 }

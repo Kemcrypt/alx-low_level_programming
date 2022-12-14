a9076be853a98022de150d5374c3343ef9011288
Kemcrypt <kemiola89@yahoo.com>
2022-12-14 03:44:05 -0800

    hot fix

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - causes an infinite loop
  * Return: 0
@@ -10,13 +11,13 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 
-i = 0;
- /*while (i < 10)*/
- /*{*/
- /* putchar(i);*/
- /*}*/
+	i = 0;
+	/*while (i < 10)*/
+	/*{*/
+	/* putchar(i);*/
+	/*}*/
 
-printf("Infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
-return (0);
+	return (0);
 }

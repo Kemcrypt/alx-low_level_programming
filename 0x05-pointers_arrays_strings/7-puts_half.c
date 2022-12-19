a55bee11fac77d142283e155526e6f701b08c702
Kemcrypt <kemiola89@yahoo.com>
2022-12-19 15:32:03 -0800

    putshalf

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,13 +14,16 @@ void puts_half(char *str)
 	longi = 0;
 
 	for (a = 0; str[a] != '\0'; a++)
-		longi++;
-		n = (longi / 2);
+	longi++;
+	n = (longi / 2);
 
 	if ((longi % 2) == 1)
-		n = ((longi + 1) / 2);
+	n = ((longi + 1) / 2);
 
 	for (a = n; str[a] != '\0'; a++)
-		_putchar(str[a]);
-		_putchar('\n');
+
+	_putchar(str[a]);
+
+	_putchar('\n');
+
 }

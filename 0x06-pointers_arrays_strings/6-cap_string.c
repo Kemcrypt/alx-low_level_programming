bca09a11c1c9f1c16a0c3095b6a4035cb6a8f896
Kemcrypt <kemiola89@yahoo.com>
2022-12-21 13:03:36 -0800

    6 cap strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,25 +13,29 @@ char *cap_string(char *str)
 
 	while (str[index])
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-			if (str[index - 1] == ' ' ||
-
-				str[index - 1] == '\t' ||
-				str[index - 1] == '\n' ||
-				str[index - 1] == ',' ||
-				str[index - 1] == ';' ||
-				str[index - 1] == '.' ||
-				str[index - 1] == '!' ||
-				str[index - 1] == '?' ||
-				str[index - 1] == '"' ||
-				str[index - 1] == '(' ||
-				str[index - 1] == ')' ||
-				str[index - 1] == '{' ||
-				str[index - 1] == '}' ||
-				index == 0)
-				str[index] -= 32;
+	while (!(str[index] >= 'a' && str[index] <= 'z'))
 		index++;
+
+	if (str[index - 1] == ' ' ||
+
+	str[index - 1] == '\t' ||
+	str[index - 1] == '\n' ||
+	str[index - 1] == ',' ||
+	str[index - 1] == ';' ||
+	str[index - 1] == '.' ||
+	str[index - 1] == '!' ||
+	str[index - 1] == '?' ||
+	str[index - 1] == '"' ||
+	str[index - 1] == '(' ||
+	str[index - 1] == ')' ||
+	str[index - 1] == '{' ||
+	str[index - 1] == '}' ||
+
+	index == 0)
+
+	str[index] -= 32;
+
+	index++;
 	}
 
 	return (str);

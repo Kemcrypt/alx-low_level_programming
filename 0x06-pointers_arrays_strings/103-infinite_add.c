ed02f7f79d2c3e11f3c2df0a626f918b23f20b56
Kemcrypt <kemiola89@yahoo.com>
2022-12-22 10:32:09 -0800

    103 infinite

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -5,10 +5,13 @@
  * @n: integer params
  * Return: 0
  */
+
 void rev_string(char *n)
 {
 	int i = 0;
+
 	int j = 0;
+	
 	char temp;
 
 	while (*(n + i) != '\0')
@@ -33,9 +36,11 @@ void rev_string(char *n)
  * @size_r: buffer size
  * Return: pointer to calling function
  */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int overflow = 0, i = 0, j = 0, digits = 0;
+
 	int val1 = 0, val2 = 0, temp_tot = 0;
 
 	while (*(n1 + i) != '\0')

51e354144634d2089871b01d0c6ece279336d9d3
Kemcrypt <kemiola89@yahoo.com>
2023-01-04 04:06:46 -0800

    5 sqrt recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,7 +14,8 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 	return (actual_sqrt_recursion(n, 0));
-	{
+}
+
 /**
  * actual_sqrt_recursion - recurses to find the natural
  * square root of a number

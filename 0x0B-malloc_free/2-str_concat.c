8c6f6f36b916ae7b7cd4f1dd2e53065accda62bc
Kemcrypt <kemiola89@yahoo.com>
2023-01-09 07:22:40 -0800

    strconcat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,18 +10,22 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *conct;
+
 	int i, ci;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
-
 		i = ci = 0;
+
 	while (s1[i] != '\0')
 		i++;
+
 	while (s2[ci] != '\0')
 		ci++;
+
 	conct = malloc(sizeof(char) * (i + ci + 1));
 
 	if (conct == NULL)

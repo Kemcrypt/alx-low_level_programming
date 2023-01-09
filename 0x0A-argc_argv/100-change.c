c9f7fc4afff854c90d0443b34cd284f9c0a9f117
Kemcrypt <kemiola89@yahoo.com>
2023-01-09 03:46:46 -0800

    100 change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,12 +13,14 @@
 int main(int argc, char *argv[])
 {
 	int num, j, result;
+
 	int coins[] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
 	{
 
 		printf("Error\n");
+
 		return (1);
 	}
 
@@ -33,6 +35,7 @@ int main(int argc, char *argv[])
 	}
 
 	printf("%d\n", result);
+
 	return (0);
 }
 

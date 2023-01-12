49d336fb5fef36af89c4f0741e9e0a4a4bf44401
Kemcrypt <kemiola89@yahoo.com>
2023-01-12 10:40:29 -0800

    5 functions

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -28,8 +28,9 @@ int _isNumber(char *argv)
 	int i;
 
 	for (i = 0; argv[i]; i++)
+
 		if (argv[i] < 48 || argv[i] > 57)
-	return (1);
+			return (1);
 	return (0);
 }
 
@@ -60,7 +61,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 * mul_array - multiply two arrays.
 * @a1: first array.
 * @len1: length of array a1.
-* @a2:  char. 
+* @a2: char.
 * @a3: array for result.
 * @lena: length of array a3.
 * Return: pointer to array.
@@ -104,7 +105,7 @@ void print_array(char *a, int nb)
 	{
 	i++;
 	}
-	
+
 	for (; i < nb; i++)
 	{
 		putchar(a[i]);
@@ -120,6 +121,7 @@ void print_array(char *a, int nb)
  */
 
 int main(int argc, char *argv[])
+
 {
 	int i, c, len1, len2, lenres;
 
@@ -129,28 +131,34 @@ int main(int argc, char *argv[])
 
 	if (argc != 3 || _isNumber(argv[1]) == 1 || _isNumber(argv[2]) == 1)
 	{
-	
-		for (i = 0; i < 6; i++);
-		{																_putchar(E[i]);													}
-			exit(98);
+		for (i = 0; i < 6; i++)
+		{
+			_putchar(E[i]);
+		}
+		exit(98);
 	}
 
-		for (len1 = 0; argv[1][len1]; len1++);
-				
-		for (len2 = 0; argv[2][len2]; len2++);
-				lenres = len1 + len2;
+		for (len1 = 0; argv[1][len1]; len1++)
+		;
+		for (len2 = 0; argv[2][len2]; len2++)
+		;
+		lenres = len1 + len2;
 		tabres = _calloc(lenres, sizeof(int));
 
 		if (tabres == NULL)
-		{																free(tabres);															return (0);
+		{
+		free(tabres);
+		return (0);
 		}
 
 		for (i = len2 - 1, c = 0; i >= 0; i--)
-		{															tabres = mul_array(argv[1], len1, argv[2][i], tabres, (lenres - 1 - c));						c++;
+		{
+		tabres = mul_array(argv[1], len1, argv[2][i], tabres, (lenres - 1 - c));
+		c++;
 		}
 		print_array(tabres, lenres);
 		free(tabres);
-		
+
 		exit(EXIT_SUCCESS);
 
 		return (0);

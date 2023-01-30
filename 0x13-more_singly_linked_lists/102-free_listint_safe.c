62a8acd4d0a5efa15781570595f396c893ada6dc
kemcrypt <kemiola89@yahoo.com>
2023-01-30 10:08:22 -0800

    102 free listlint

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -15,10 +15,10 @@ size_t free_listint_safe(listint_t **h)
 
 	if (!h || !*h)
 		return (0);
-	
+
 	while (*h)
 	{
-		diff = *h - (*h)->next
+		diff = *h - (*h)->next;
 		if (diff > 0)
 		{
 			temp = (*h)->next;

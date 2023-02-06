f62ac7031b93597cda8bf11ae138e8cc306ab5bb
kemcrypt <kemiola89@yahoo.com>
2023-02-06 12:09:44 -0800

    3 cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -73,7 +73,7 @@ int main(int argc, char *argv[])
 	buffer = create_buffer(argv[2]);
 	from = open(argv[1], O_RDONLY);
 	r = read(from, buffer, 1024);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664)
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 		do {
 			if (from == -1 || r == -1)

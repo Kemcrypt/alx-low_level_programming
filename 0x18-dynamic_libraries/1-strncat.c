56feb8a0208ef0158dc5a1a5b3e51729fa5ac13c
kemcrypt <kemiola89@yahoo.com>
2023-03-20 22:22:20 +0100

    Create 1-strncat.c

---
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings.
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Description: Write a function that concatenates two strings.
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}

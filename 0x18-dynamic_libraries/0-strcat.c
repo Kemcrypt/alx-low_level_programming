a8481b4420521171132752f386f4f78bdbf06aed
kemcrypt <kemiola89@yahoo.com>
2023-03-20 22:18:44 +0100

    Create 0-strcat.c

---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcat - concantenates two strings
+ * @dest: char
+ * @src: char
+ *
+ * Description: Write a function that concatenates two strings.
+ *
+ * Return: a char
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}

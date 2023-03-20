1ffc0e1e5ae65fe9a5c3fc7732788a3c80511eb8
kemcrypt <kemiola89@yahoo.com>
2023-03-20 22:32:43 +0100

    Create 6-abs.c

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute value of an integer
+ *  @n: n is an integer
+ *
+ *  Description: prints the absolute value of an integer
+ *
+ *  Return: Always 0.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+		return (-n);
+}

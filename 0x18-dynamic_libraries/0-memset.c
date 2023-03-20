b153edf2f2bd016eec06f1686614854bfebc1ca4
kemcrypt <kemiola89@yahoo.com>
2023-03-20 22:17:59 +0100

    Create 0-memset.c

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset - fill a block of memory with a specific value
+ * @s: starting address of memory to be filled
+ * @b: the desired value
+ * @n: number of bytes to be changed
+ *
+ * Description: Write a function that fills memory with a constant byte
+ *
+ * Return: changed array with new value for n bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}

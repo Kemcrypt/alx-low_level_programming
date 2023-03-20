f889eafc9ef900a09707d69a2523de5963da3fee
kemcrypt <kemiola89@yahoo.com>
2023-03-20 22:27:02 +0100

    Create 2-strncpy.c

---
+++ 0x18-dynamic_libraries/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncpy -  copies a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Description: Write a function that copies a string
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
+}
